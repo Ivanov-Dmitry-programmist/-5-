f750b9dd9d9e50dcfe275c67056e3526a97e20ed
Ivanov-Dmitry-programmist <d89654216069@yandex.ru>
2025-02-04 21:40:47 +0300

    Update main.go

--- main.go
+++ main.go
@@ -29,7 +29,7 @@ Weight		float64       			// вес пользователя в кг
 // количество_повторов * длина_шага / м_в_км
 func (t Training) distance() float64 {
 	// вставьте ваш код ниже
-	return (Action * LenStep) / MInKm
+	return (t.Action * LenStep) / MInKm
 }
 
 // meanSpeed возвращает среднюю скорость бега или ходьбы.
