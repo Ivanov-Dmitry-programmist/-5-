fc9e467de42ebeadf925ec064a05588c8dfcd320
Ivanov-Dmitry-programmist <d89654216069@yandex.ru>
2025-02-05 18:38:58 +0300

    Update main.go

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func (t Training) TrainingInfo() InfoMessage {
 	return InfoMessage{
 	TrainingType:   t.TrainingType,     
 	Duration:	t.Duration, 	   
-	Distance:	t.Distance(),
+	Distance:	i.Distance(),
 	Speed:		t.meanSpeed(),    
 	Calories:	t.Calories(),
 	}
